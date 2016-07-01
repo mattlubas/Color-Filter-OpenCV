fd4031cf871b15c300eb7d89137943efa9e7335c
Matt Lubas <mlubas@fred.cs.wlu.edu>
2016-07-01 15:26:04 -0400

    Cleaned up Cod/ comments

--- ColorFilter.py
+++ ColorFilter.py
@@ -7,24 +7,17 @@ import argparse
 
 cap = cv2.VideoCapture(0)
 
+#statement below is for creating a new video.
 # Define the codec and create VideoWriter object
 # fourcc is a 4-byte code that can be found at http://www.fourcc.org/codecs.php
 '''Use for storing Video
 fourcc = cv2.cv.CV_FOURCC('M','J','P','G')
 video = cv2.VideoWriter('test.avi',fourcc, 20.0, (640,480))
 '''
-"""
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = "path to the image")
-args = vars(ap.parse_args())
-"""
 
 
 
 # define the list of color boundaires
-boundaries = [([17, 15, 100], [50, 56, 200]), ([86, 31, 4], [220, 88, 50]),([25, 146, 190], [62, 174, 250]), ([103, 86, 65], [145, 133, 128])]
-
 
 red_bound_low = [17, 15, 100]
 red_bound_high = [50, 56, 200]
@@ -43,8 +36,6 @@ while(True):
 
     # Change color options
     #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    #    NoBlue =     
-    
     #display the resulting frame
     # gray to be in grayscale
 
@@ -53,8 +44,8 @@ while(True):
     #cv2.imshow('frame', frame)
 
 
-    # find the colors within the specified boundaries and apply
-    # the mask
+    # Frame to be filtered with the colors within the specified boundaries
+    # and apply the mask
     lower = np.array(yellow_bound_low,dtype = "uint8")
   
     upper = np.array(yellow_bound_high,dtype = "uint8")
